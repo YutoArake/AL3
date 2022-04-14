483ab8a1f6b0983aa8f30f8f1b86f65a6ec2c2f8
YutoArake <k021g1420@g.neec.ac.jp>
2022-04-14 16:25:12 +0900

    02_01_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,9 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +16,41 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	// X, Y, Z 方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+
+	// X, Y, Z 軸周りの回転角を設定
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+
+	// X, Y, Z 軸周りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	
+	//デバッグテキストの表示
+	//書式指定付き表示
+	debugText_->SetPos(50, 50);
+	debugText_->Printf("translation:(%f,%f,%f)",
+		worldTransform_.translation_.x, worldTransform_.translation_.y, worldTransform_.translation_.z);
+	debugText_->SetPos(50, 70);
+	debugText_->Printf("rotation:(%f,%f,%f)",
+		worldTransform_.rotation_.x, worldTransform_.rotation_.y, worldTransform_.rotation_.z);
+	debugText_->SetPos(50, 90);
+	debugText_->Printf("scale:(%f,%f,%f)",
+		worldTransform_.scale_.x, worldTransform_.scale_.y, worldTransform_.scale_.z);
+}
 
 void GameScene::Draw() {
 
@@ -44,6 +78,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	//3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -50,5 +50,13 @@ class GameScene {
 
 	/// <summary>
 	/// ゲームシーン用
+	/// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	///3Dモデル
+	Model* model_ = nullptr;
+	///ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	/// ビュープロジェクション
+	ViewProjection viewProjection_;
 	/// </summary>
 };
