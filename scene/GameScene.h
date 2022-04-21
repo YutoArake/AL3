b6ccb2723fe9bf5c97be2fac541bf76c6a519096
YutoArake <k021g1420@g.neec.ac.jp>
2022-04-21 14:31:00 +0900

    02_02の確認課題まで

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -55,8 +55,11 @@ class GameScene {
 	///3Dモデル
 	Model* model_ = nullptr;
 	///ワールドトランスフォーム
-	WorldTransform worldTransform_;
+	WorldTransform worldTransform_[100];
 	/// ビュープロジェクション
 	ViewProjection viewProjection_;
+	///カメラ方向の角度
+	float viewAngle = 0.0f;
+
 	/// </summary>
 };
